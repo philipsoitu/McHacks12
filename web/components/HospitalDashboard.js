9a34274f55f7847efc11a29d0308acd22f4d9d76
philipsoitu <philip.soitu@gmail.com>
2025-01-25 18:53:24 -0500

    cleanup

--- web/components/HospitalDashboard.js
+++ web/components/HospitalDashboard.js
@@ -1,7 +1,5 @@
 import CameraFeed from "./CameraFeed"
 import ResourceInventory from "./ResourceInventory"
-import StaffAvailability from "./StaffAvailability"
-import EmergencyCases from "./EmergencyCases"
 
 export default function HospitalDashboard({ hospital }) {
   return (
@@ -9,15 +7,12 @@ export default function HospitalDashboard({ hospital }) {
       <div>
         <h2 className="text-2xl font-semibold mb-4">Live Camera Feeds</h2>
         <div className="grid grid-cols-2 gap-4">
-          <CameraFeed id="1" name="Main Entrance" />
-          <CameraFeed id="2" name="Emergency Room" />
-          <CameraFeed id="3" name="Waiting Area 1" />
-          <CameraFeed id="4" name="Waiting Area 2" />
+          <CameraFeed id="1" name="Waiting Area 1" />
+          <CameraFeed id="2" name="Waiting Area 2" />
         </div>
       </div>
       <div>
         <ResourceInventory hospitalId={hospital.id} />
-        <EmergencyCases hospitalId={hospital.id} />
       </div>
     </div>
   )
