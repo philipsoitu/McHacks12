280d082c52cc667bd43fc194b55013c04583825f
philipsoitu <philip.soitu@gmail.com>
2025-01-26 10:23:58 -0500

    poggers

--- web/components/HospitalDashboard.js
+++ web/components/HospitalDashboard.js
@@ -9,6 +9,15 @@ export default function HospitalDashboard({ hospital }) {
         <div className="grid grid-cols-2 gap-4">
           <CameraFeed id="1" name="Waiting Area 1" />
           <CameraFeed id="2" name="Waiting Area 2" />
+          <div style={{ position: 'relative', paddingTop: '56.25%' }}>
+            <iframe
+              src={`https://player.twitch.tv/?channel=raccoonseb4&parent=yourdomain.com`}
+              height="100%"
+              width="100%"
+              allowFullScreen
+              style={{ position: 'absolute', top: 0, left: 0 }}
+            ></iframe>
+          </div>
         </div>
       </div>
       <div>
